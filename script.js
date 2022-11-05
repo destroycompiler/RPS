36b67e99e1bfe89361adfa651ba1629988431009
destroycompiler <dc.dhruvchouhan@gmail.com>
2022-11-05 12:24:00 +0530

    Minor improvements in the game code
    - proper comments added
    - score output at the end of the game
    - slightly more efficient

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+// gets random choice from the computer {0, 1, 2}
+// according to the number, it selects the choice rock paper of scissor
+
 function getComputerChoice() {
     let choice = Math.random();
     choice = Math.floor(choice*10);
@@ -12,54 +16,64 @@ function getComputerChoice() {
     return choiceList[choice];
 }
 
+// setting the player's score and ties if there might be any
+let playerScore = 0;
+let ties = 0;
+
+// mechanics of playing the round
+
 function playRound(playerSelection, computerSelection) {
+
+    // to make case insensitive
     playerSelection = playerSelection.toLowerCase();
 
-    // computerSelection = computerSelection.toLowerCase();
-    // you lose
+    // valid choice check
 
     if(playerSelection == 'rock' || playerSelection == 'scissor' || playerSelection == 'paper') {
 
+        // you lose
         if(
             (playerSelection == 'rock' && computerSelection == 'paper') ||
             (playerSelection == 'scissor' && computerSelection == 'rock') ||
             (playerSelection == 'paper' && computerSelection == 'scissor')
         ) {
-            return (`YOU LOSE !! ${computerSelection} beats ${playerSelection}`);
+            return (`YOU LOSE !! computer : ${computerSelection} beats player : ${playerSelection}`);
         }
-
         // you win
         if(
             (playerSelection == 'rock' && computerSelection == 'scissor') ||
             (playerSelection == 'scissor' && computerSelection == 'paper') ||
             (playerSelection == 'paper' && computerSelection == 'rock')
         ) {
-           return (`YOU WIN !! ${playerSelection} beats ${computerSelection}`);
+           playerScore++;
+           return (`YOU WIN !! player : ${playerSelection} beats computer : ${computerSelection}`);
         } 
         
         // tie game
         else {
+            ties++;
             return ("TIED !!");
-            // playRound(playerSelection, computerSelection);
         }
 
-    } else {
+    } 
+    else {
         alert("Enter the correct option you IDIOT");
+        
+        // plays round again if not valid input
         playTheRound();
 
     }
 
 }
 
+// play round with the input from the user
 
 function playTheRound() {
         getComputerChoice();
 
         let playerSelection = (prompt("Enter your choice : rock paper or scissor").trim());
 
-        // main
-        // let playerSelection = 'rock';
-        // let playerSelection = prompt("Enter your choice, rock, paper or scissor");
+        // compatible with whitespace using string.trim();
 
         playerSelection = playerSelection.trim();
         let computerSelection = getComputerChoice();
@@ -68,8 +82,8 @@ function playTheRound() {
 
 }
 
+// play the required number of rounds
 function game(rounds) {
-
 
     for (let i = 0; i < rounds; i++) {
         playTheRound();
@@ -80,23 +94,18 @@ function game(rounds) {
 
 let rounds;
 
+// check for valid number input value
+
 while (typeof(rounds) !== typeof(5)) {
     rounds = parseInt(((prompt("Enter the number of rounds")).trim()));
 }
 
-// console.log(rounds);
-// console.log(typeof rounds)
-
+// final game execution
 game(rounds);
 
-
-
-
-
-
-
-
-
+// print the score
+console.log(`Your score is ${playerScore} out of ${rounds}`);
+console.log("Number of ties = " + ties);
 
 
 
